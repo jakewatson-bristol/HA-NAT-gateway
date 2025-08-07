2e35b75e7f3c239a1d8f73634f311dc4338d29a0
Jake Watson <jake.watson@bristol.ac.uk>
2025-08-07 20:02:42 +0100

    remove sshd and dynamically assign conntracd interface

--- natgateway/gateway.sh
+++ natgateway/gateway.sh
@@ -1,57 +1,49 @@
 #!/bin/bash
 # Gateway script for NAT instances
 # This script sets up the NAT gateway with keepalived and conntrackd
+
+# remove the lock file if it exists
+if [ -f /var/lock/conntrackd.lock ]; then
+    rm /var/lock/conntrackd.lock
+fi
+
 while true; do
+    # Start conntrackd
+    # Detect which nat gateway instance this is
+    if [ "$HOSTNAME" == "nat1" ]; then
+        NET2_ETH=$(ip -o -4 addr show | awk '/172\.24\.0\.10/ {print $2}')
+    else
+        NET2_ETH=$(ip -o -4 addr show | awk '/172\.24\.0\.11/ {print $2}')
+    fi
     TRY_COUNT=0
-    while ! pgrep sshd >/dev/null; do
-        # wait for the SSH daemon to start
-        if pgrep sshd >/dev/null; then
-            echo "SSH daemon is running"
-            break
-        else
-            echo "Waiting for SSH daemon to start..."
-            sleep 1
-            # Start the SSH daemon if it's not running
-            /usr/sbin/sshd
+    while ! pgrep conntrackd >/dev/null; do
+        echo "conntrackd is not running, trying to start it..."
+        sleep 1
+        if ! pgrep conntrackd >/dev/null; then
+            # Substitute the interface in the configuration file
+            sed "s/__NET2_ETH__/$NET2_ETH/g" /etc/conntrackd/conntrackd.$HOSTNAME.conf > /etc/conntrackd/conntrackd.conf
+            conntrackd -C /etc/conntrackd/conntrackd.conf -d
             TRY_COUNT=$((TRY_COUNT + 1))
-            echo "Attempt $TRY_COUNT to start SSH daemon"
-        fi
-        if [ $TRY_COUNT -ge 5 ]; then
-            echo "SSH daemon failed to start after 5 attempts, exiting..."
-            echo "skipping SSH setup for $HOSTNAME"
-            break
+            echo "Attempt $TRY_COUNT to start conntrackd"
+            if [ $TRY_COUNT -ge 5 ]; then
+                echo "Failed to start conntrackd after 5 attempts, exiting..."
+                echo "skipping conntrackd setup for $HOSTNAME"
+                break
+            fi
         fi
     done
-    # Use $HOSTNAME to select config files directly
+    # Start keepalived
     TRY_COUNT=0
     while ! pgrep keepalived >/dev/null; do
-        echo "Waiting for keepalived to start..."
+        echo "keepalived is not running, trying to start it..."
         sleep 1
         if ! pgrep keepalived >/dev/null; then
-            echo "keepalived is not running, trying to start it again..."
             keepalived -D -f /etc/keepalived/keepalived.$HOSTNAME.conf
             TRY_COUNT=$((TRY_COUNT + 1))
             echo "Attempt $TRY_COUNT to start keepalived"
             if [ $TRY_COUNT -ge 5 ]; then
                 echo "Failed to start keepalived after 5 attempts, exiting..."
                 echo "skipping keepalived setup for $HOSTNAME"
-                break
-            fi
-        fi
-    done
-
-    TRY_COUNT=0
-    while ! pgrep conntrackd >/dev/null; do
-        echo "Waiting for conntrackd to start..."
-        sleep 1
-        if ! pgrep conntrackd >/dev/null; then
-            echo "conntrackd is not running, trying to start it again..."
-            conntrackd -C /etc/conntrackd/conntrackd.$HOSTNAME.conf -d
-            TRY_COUNT=$((TRY_COUNT + 1))
-            echo "Attempt $TRY_COUNT to start conntrackd"
-            if [ $TRY_COUNT -ge 5 ]; then
-                echo "Failed to start conntrackd after 5 attempts, exiting..."
-                echo "skipping conntrackd setup for $HOSTNAME"
                 break
             fi
         fi
